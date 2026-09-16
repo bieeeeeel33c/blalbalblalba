5ccb9371acdfeb06a22d8765a34901f3f2876d8e
bieeeeeel33c <bieelcorrea540@gmail.com>
2026-09-15 21:25:26 -0300

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -114,7 +114,7 @@ end)
 
 local qt = queue_on_teleport or queueteleport or (syn and syn.queue_on_teleport)
 if qt then
-    qt([[loadstring(game:HttpGet("https://raw.githubusercontent.com/SEU_USER/SEU_REPO/main/game.lua"))()]])
+    qt([[loadstring(game:HttpGet("https://raw.githubusercontent.com/bieeeeeel33c/blalbalblalba/refs/heads/main/game.lua"))()]])
 end
 
 RunService.Heartbeat:Connect(function()
